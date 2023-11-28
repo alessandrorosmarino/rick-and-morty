b611f066e1977d5e71417d27727230a0fa64cb68
Alessandro Rosmarino <alessandrorosmarino@gmail.com>
2023-11-28 22:01:31 +0000

    changed episode fetch
    Changed the returning type and sign of method to better match needs

--- src/main/java/com/bcnc/rick/morty/consumer/web/service/RMEpisodeService.java
+++ src/main/java/com/bcnc/rick/morty/consumer/web/service/RMEpisodeService.java
@@ -6,8 +6,8 @@ import org.springframework.stereotype.Service;
 @Service
 public class RMEpisodeService {
 
-    public Episode getEpisodeById(int id) {
+    public Episode[] getEpisodesByIds(Integer... ids) {
         Episode episode = new Episode();
-        return episode.get(id).toArray(new Episode[0])[0];
+        return episode.get(ids).toArray(new Episode[0]);
     }
 }
