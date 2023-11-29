0e5b502c79cec7eaae7fe1484111ee8f354d568b
Alessandro Rosmarino <alessandrorosmarino@gmail.com>
2023-11-29 22:41:21 +0000

    updated RMController
    updated RMController to use the RMConsumerService

--- src/main/java/com/bcnc/rick/morty/consumer/web/controller/RMController.java
+++ src/main/java/com/bcnc/rick/morty/consumer/web/controller/RMController.java
@@ -1,15 +1,26 @@
 package com.bcnc.rick.morty.consumer.web.controller;
 
 import com.bcnc.rick.morty.consumer.web.entity.RMCharacterData;
+import com.bcnc.rick.morty.consumer.web.service.RMConsumerService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class RMController {
 
+    private RMConsumerService rmConsumerService;
+
+    @Autowired
+    public RMController(RMConsumerService rmConsumerService) {
+        this.rmConsumerService = rmConsumerService;
+    }
+
     @GetMapping("/search-character-appearance")
-    public RMCharacterData getRickAndMorty(@RequestParam(name = "name") String name) {
-        return new RMCharacterData();
+    public List<RMCharacterData> getRickAndMorty(@RequestParam(name = "name") String name) {
+        return rmConsumerService.getCharacterForConsumerByName(name);
     }
 }
