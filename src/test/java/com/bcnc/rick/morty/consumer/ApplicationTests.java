5528912e1e4eac4bff43ca10ee4419ea96ad73d3
Alessandro Rosmarino <alessandrorosmarino@gmail.com>
2023-11-29 22:51:43 +0000

    added test to ApplicationTests
    Added the exercise test to the ApplicationTests class

--- src/test/java/com/bcnc/rick/morty/consumer/ApplicationTests.java
+++ src/test/java/com/bcnc/rick/morty/consumer/ApplicationTests.java
@@ -1,13 +1,25 @@
 package com.bcnc.rick.morty.consumer;
 
+import com.bcnc.rick.morty.consumer.web.entity.RMCharacterData;
+import com.bcnc.rick.morty.consumer.web.service.RMConsumerService;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 class ApplicationTests {
 
+	@Autowired
+	private RMConsumerService rmConsumerService;
+
 	@Test
-	void contextLoads() {
+	void test_consumer_service_should_get_4_characters() {
+		List<RMCharacterData> rmCharacterDataList = rmConsumerService.getCharacterForConsumerByName("Rick Sanchez");
+		System.out.println(rmCharacterDataList);
+		Assertions.assertEquals(4, rmCharacterDataList.size());
 	}
 
 }
