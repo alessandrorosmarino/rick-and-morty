87c10e5a0b97dbc83fc46e9506dd38998f954eee
Alessandro Rosmarino <alessandrorosmarino@gmail.com>
2023-11-29 22:55:35 +0000

    Fixed RMEpisodeService
    Fixed a problem with the get method casting JsonObject to JsonArray (caused by the Rick And Morty Java Client)

--- src/main/java/com/bcnc/rick/morty/consumer/web/service/RMEpisodeService.java
+++ src/main/java/com/bcnc/rick/morty/consumer/web/service/RMEpisodeService.java
@@ -3,11 +3,18 @@ package com.bcnc.rick.morty.consumer.web.service;
 import com.rickandmortyapi.Episode;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 public class RMEpisodeService {
 
     public Episode[] getEpisodesByIds(Integer... ids) {
-        Episode episode = new Episode();
-        return episode.get(ids).toArray(new Episode[0]);
+        List<Episode> episodes = new ArrayList<>();
+        for (Integer id : ids) {
+            Episode episode = new Episode(id);
+            episodes.add(episode.refresh());
+        }
+        return episodes.toArray(new Episode[0]);
     }
 }
