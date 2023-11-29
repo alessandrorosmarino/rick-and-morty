6b142bfafaf07262e2c59a259df4fe5371aaa87c
Alessandro Rosmarino <alessandrorosmarino@gmail.com>
2023-11-29 23:25:42 +0000

    documented RMCharacterService

--- src/main/java/com/bcnc/rick/morty/consumer/web/service/RMCharacterService.java
+++ src/main/java/com/bcnc/rick/morty/consumer/web/service/RMCharacterService.java
@@ -3,10 +3,13 @@ package com.bcnc.rick.morty.consumer.web.service;
 import com.rickandmortyapi.Character;
 import org.springframework.stereotype.Service;
 
+/***
+ * This class is the service that will be used to consume the Rick and Morty API Character endpoint.
+ */
 @Service
 public class RMCharacterService {
 
-    public Character[] getCharacterByName(String name) {
+    protected Character[] getCharacterByName(String name) {
         Character character = new Character();
         return character.withName(name).filter().toArray(new Character[0]);
     }
