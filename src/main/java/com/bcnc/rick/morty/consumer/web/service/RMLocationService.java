07e8b72837e8c48736c2696ab9a4babf9c093ee1
Alessandro Rosmarino <alessandrorosmarino@gmail.com>
2023-11-29 23:26:04 +0000

    documented RMLocationService

--- src/main/java/com/bcnc/rick/morty/consumer/web/service/RMLocationService.java
+++ src/main/java/com/bcnc/rick/morty/consumer/web/service/RMLocationService.java
@@ -3,10 +3,13 @@ package com.bcnc.rick.morty.consumer.web.service;
 import com.rickandmortyapi.Location;
 import org.springframework.stereotype.Service;
 
+/***
+ * This class is the service that will be used to consume the Rick and Morty API Location endpoint.
+ */
 @Service
 public class RMLocationService {
 
-    public Location getLocationById(Integer id) {
+    protected Location getLocationById(Integer id) {
         Location location = new Location(id);
         return location.refresh();
     }
