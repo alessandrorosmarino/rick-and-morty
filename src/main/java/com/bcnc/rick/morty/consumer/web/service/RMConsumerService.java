c0d1876f68788aff5234b162b9031803a56223e7
Alessandro Rosmarino <alessandrorosmarino@gmail.com>
2023-11-29 22:40:00 +0000

    updated Consumer service
    Updated RMConsumerService to use the consumer character object

--- src/main/java/com/bcnc/rick/morty/consumer/web/service/RMConsumerService.java
+++ src/main/java/com/bcnc/rick/morty/consumer/web/service/RMConsumerService.java
@@ -1,13 +1,16 @@
 package com.bcnc.rick.morty.consumer.web.service;
 
-import com.google.gson.JsonArray;
-import com.google.gson.JsonObject;
+import com.bcnc.rick.morty.consumer.web.entity.RMCharacterData;
 import com.rickandmortyapi.Character;
 import com.rickandmortyapi.Episode;
 import com.rickandmortyapi.Location;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.threeten.bp.format.DateTimeFormatter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 @Service
 public class RMConsumerService {
@@ -24,26 +27,24 @@ public class RMConsumerService {
         this.rmLocationService = rmLocationService;
     }
 
-    public JsonArray getCharacterForConsumerByName(String name) {
-        JsonArray characterJsonArray = new JsonArray();
+    public List<RMCharacterData> getCharacterForConsumerByName(String name) {
+        List<RMCharacterData> rmCharacterDataList = new ArrayList<>();
         Character[] characters = rmCharacterService.getCharacterByName(name);
         for (Character character : characters) {
-            JsonObject characterJsonObject = new JsonObject();
-            characterJsonObject.addProperty("name",character.getName());
+            RMCharacterData rmCharacterData = new RMCharacterData();
+            rmCharacterData.setName(character.getName());
             Integer[] episodeIds = character.getEpisodes().stream().map(
                     Episode::getId
             ).toArray(Integer[]::new);
             Episode[] episodes = rmEpisodeService.getEpisodesByIds(episodeIds);
-            JsonArray episodeArray = new JsonArray();
-            for (Episode episode : episodes) {
-                episodeArray.add(episode.getName());
-            }
-            characterJsonObject.add("episodes",episodeArray);
-            Location location = rmLocationService.getLocationByIds(character.getOriginLocation().getId())[0];
-            characterJsonObject.addProperty("first_appearance",
-                    location.getCreated().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME));
-            characterJsonArray.add(characterJsonObject);
+            rmCharacterData.setEpisodes(Arrays.stream(episodes).map(
+                    Episode::getName
+            ).toArray(String[]::new));
+            Location location = rmLocationService.getLocationById(character.getOriginLocation().getId());
+            rmCharacterData.setFirstAppearance(
+                    location.getCreated().format(DateTimeFormatter.ISO_LOCAL_DATE));
+            rmCharacterDataList.add(rmCharacterData);
         }
-        return characterJsonArray;
+        return rmCharacterDataList;
     }
 }
