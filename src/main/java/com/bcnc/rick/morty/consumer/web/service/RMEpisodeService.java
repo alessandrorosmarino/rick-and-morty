06a109316dce219c1dc97332568755b4f171a8f6
Alessandro Rosmarino <alessandrorosmarino@gmail.com>
2023-11-29 23:25:55 +0000

    documented RMEpisodeService

--- src/main/java/com/bcnc/rick/morty/consumer/web/service/RMEpisodeService.java
+++ src/main/java/com/bcnc/rick/morty/consumer/web/service/RMEpisodeService.java
@@ -6,10 +6,13 @@ import org.springframework.stereotype.Service;
 import java.util.ArrayList;
 import java.util.List;
 
+/***
+ * This class is the service that will be used to consume the Rick and Morty API Episode endpoint.
+ */
 @Service
 public class RMEpisodeService {
 
-    public Episode[] getEpisodesByIds(Integer... ids) {
+    protected Episode[] getEpisodesByIds(Integer... ids) {
         List<Episode> episodes = new ArrayList<>();
         for (Integer id : ids) {
             Episode episode = new Episode(id);
