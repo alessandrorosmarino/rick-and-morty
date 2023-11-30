f95fb5bb9a24f29dd23a9d32de44a1d5eca8e0ff
Alessandro Rosmarino <alessandrorosmarino@gmail.com>
2023-11-30 00:13:14 +0000

    updated tests
    Changed tests from services to controller and added a few tests not provided by the exercise

--- src/test/java/com/bcnc/rick/morty/consumer/ApplicationTests.java
+++ src/test/java/com/bcnc/rick/morty/consumer/ApplicationTests.java
@@ -1,7 +1,8 @@
 package com.bcnc.rick.morty.consumer;
 
+import com.bcnc.rick.morty.consumer.exception.NoRMCharacterFoundException;
+import com.bcnc.rick.morty.consumer.web.controller.RMController;
 import com.bcnc.rick.morty.consumer.web.entity.RMCharacterData;
-import com.bcnc.rick.morty.consumer.web.service.RMConsumerService;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,13 +14,37 @@ import java.util.List;
 class ApplicationTests {
 
 	@Autowired
-	private RMConsumerService rmConsumerService;
+	private RMController rmController;
 
+	/***
+	 * This test case is to verify the controller is returning the correct number of characters
+	 */
 	@Test
-	void test_consumer_service_should_get_4_characters() {
-		List<RMCharacterData> rmCharacterDataList = rmConsumerService.getCharacterForConsumerByName("Rick Sanchez");
+	void test_controller_should_get_4_characters() {
+		List<RMCharacterData> rmCharacterDataList = rmController.getRickAndMorty("Rick Sanchez");
+		//printing the object to verify the content
 		System.out.println(rmCharacterDataList);
 		Assertions.assertEquals(4, rmCharacterDataList.size());
+	}
+
+	/***
+	 * This test case is to verify the controller is throwing the correct exception when no character is found
+	 */
+	@Test
+	void test_controller_gibberish_data_no_Character_found() {
+		Assertions.assertThrows(NoRMCharacterFoundException.class,
+				() -> rmController.getRickAndMorty("gibberishdata"));
+	}
+
+	/***
+	 * This test case is to verify the controller is returning the correct number of characters when no name is provided
+	 */
+	@Test
+	void test_controller_without_name_value() {
+		List<RMCharacterData> rmCharacterDataList = rmController.getRickAndMorty("");
+		//printing the object to verify the content
+		System.out.println(rmCharacterDataList);
+		Assertions.assertEquals(20, rmCharacterDataList.size());
 	}
 
 }
